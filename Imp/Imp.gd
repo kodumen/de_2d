2dcd981deb246f6b0b8cd202e5852fcefbdf2c2f
kodumen <r.b.daitan@gmail.com>
2021-07-31 14:02:49 +0800

    Implement imp sightlines
    It works good enough. We can proceed with the next phase. I think the
    sightline logic should be included in the base Enemy class, but I'm
    waiting until I have to implement it on another enemy type before moving
    it.

--- Imp/Imp.gd
+++ Imp/Imp.gd
@@ -2,21 +2,45 @@ extends Enemy
 
 
 var ranged_attack_zone_collider: CollisionShape2D
-
+var sightline:RayCast2D
+var is_target_in_range = false
 
 func _enemy_ready():
 	path_line = $Line2D
 	animated_sprite = $AnimatedSprite
 	hit_box = $HitBox
 	ranged_attack_zone_collider = $RangedAttackZone/CollisionShape2D
+	sightline = $Sightline
+
+
+func _process(_delta):
+	if state == STATE.DEAD:
+		return
+		
+	if target:
+		sightline.look_at(target.global_position)
+		
+		
+func _physics_process(_delta):
+	if state == STATE.DEAD:
+		return
+		
+	if state != STATE.ATTACKING and is_target_in_range and is_sightline_clear():
+		set_state(STATE.ATTACKING)
 
 
 func _on_RangedAttackZone_body_entered(body):
-	if body == target and state != STATE.DEAD:
+	if target == body:
+		is_target_in_range = true
+		
+	if body == target and state != STATE.DEAD and is_sightline_clear():
 		set_state(STATE.ATTACKING)
 
 
 func _on_RangedAttackZone_body_exited(body):
+	if target == body:
+		is_target_in_range = false
+		
 	if body == target and state != STATE.DEAD:
 		set_state(STATE.MOVING)
 
@@ -24,3 +48,7 @@ func _on_RangedAttackZone_body_exited(body):
 func _on_Enemy_state_dead():
 	._on_Enemy_state_dead()
 	ranged_attack_zone_collider.set_deferred("disabled", true)
+	
+	
+func is_sightline_clear():
+	return ! sightline.is_colliding()
